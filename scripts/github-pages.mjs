ce4e7c17303cdff2e0a4c62ecd2e1a29971ff5cd
VisualSource <collin_blosser@yahoo.com>
2025-01-21 13:56:25 -0500

    update html file

--- scripts/github-pages.mjs
+++ scripts/github-pages.mjs
@@ -1,90 +1,71 @@
-import {
-  readdir,
-  writeFile,
-  readFile,
-  copyFile,
-  mkdir,
-} from "node:fs/promises";
+// scripts/github-pages.mts
+import { readdir, writeFile, readFile, copyFile, mkdir } from "node:fs/promises";
+import { fileURLToPath } from "node:url";
 import { existsSync } from "node:fs";
 import { join } from "node:path";
-import { fileURLToPath } from "node:url";
-
-function resolve(path) {
+function reslove(path) {
   return fileURLToPath(import.meta.resolve(path));
 }
-
-const INPUT_DIR = resolve("../packages");
-const OUTPUT_DIR = resolve("../dist");
-const OUTPUT_SCREENSHOTS = join(OUTPUT_DIR, "screenshots");
-const OUTPUT_HTML = join(OUTPUT_DIR, "index.html");
-const HTML_TEMPLETE = resolve("./index.html");
-
-const list_item = /*html*/ `
+var paths = {
+  INPUT: {
+    PROJECTS: reslove("../packages"),
+    TEMPLETE: reslove("./index.html")
+  },
+  OUTPUTS: {
+    DIR: reslove("../dist"),
+    SCREENSHOTS: "screenshots",
+    HTML: reslove("index.html")
+  }
+};
+var imageItem = `
     <li>
         <div style="margin-bottom: 10px; margin-top: 20px;">
-          <span> {{ project_name }} |</span>
-          <a href="{{ demo_link }}" target="_blank">View Demo</a>
+          <span>{{project_name}} |</span>
+          <a href="{{demo_link}}" target="_blank">View Demo</a>
         </div>
-        <img src="{{ screenshot }}" alt="{{ screenshot_alt }}"/>
+        <img src="{{screenshot}}" alt="{{screenshot_alt}}"/>
     </li>
 `;
-/*
-    1. Read html content from index.html file
-    2. read dist dir to get project names
-    3. generate links to the projects
-    4. copy screenshots for project to dist
-    5. write html to dist
-*/
+var linkItem = `
+  <li>
+     <div style="margin-bottom: 10px; margin-top: 20px;">
+        <span>{{project_name}} |</span>
+        <a href="{{link}}" target="_blank">View Page</a>
+      </div>
+  </li>
+`;
 try {
-  const html = await readFile(HTML_TEMPLETE, {
-    encoding: "utf-8",
-  });
-
-  if (!existsSync(OUTPUT_DIR)) {
-    await mkdir(OUTPUT_DIR);
-  }
-
-  if (!existsSync(OUTPUT_SCREENSHOTS)) {
-    await mkdir(OUTPUT_SCREENSHOTS);
-  }
-
-  const projects = await readdir(INPUT_DIR);
-
+  if (!existsSync(paths.OUTPUTS.DIR))
+    await mkdir(paths.OUTPUTS.DIR);
+  const screenshotDir = join(paths.OUTPUTS.DIR, paths.OUTPUTS.SCREENSHOTS);
+  if (!existsSync(screenshotDir))
+    await mkdir(screenshotDir);
+  const outputScreenshots = join(paths.OUTPUTS.DIR, paths.OUTPUTS.SCREENSHOTS);
   const links = [];
-
-  for (const project of projects) {
-    const screen_shot = join(INPUT_DIR, `${project}/screenshot.png`);
-    if (!existsSync(screen_shot)) {
+  for (const project of await readdir(paths.INPUT.PROJECTS)) {
+    const url = `/${project}`;
+    const name = project.split("-").map((e, i) => e.replace(/^./, e[0].toUpperCase())).join(" ");
+    const screenshot = join(paths.INPUT.PROJECTS, `${project}/screenshot.png`);
+    if (!existsSync(screenshot)) {
+      links.push({
+        templete: linkItem.replace("{{project_name}}", name).replace("{{link}}", url),
+        type: 1
+      });
       continue;
     }
-
-    const url = `/${project}`;
-    const name = project
-      .split("-")
-      .map((e, i) => {
-        if (i === 0) {
-          e = e[0].toUpperCase() + e.substring(1);
-        }
-        return e;
-      })
-      .join(" ");
-
-    await copyFile(screen_shot, join(OUTPUT_SCREENSHOTS, `${project}.png`));
-
-    let item = list_item
-      .replace("{{ project_name }}", name)
-      .replace("{{ demo_link }}", url)
-      .replace("{{ screenshot }}", `/screenshots/${project}.png`)
-      .replace("{{ screenshot_alt }}", `${project} screenshot`);
-
-    links.push(item);
+    await copyFile(screenshot, join(outputScreenshots, `${project}.png`));
+    links.push({
+      templete: imageItem.replace("{{project_name}}", name).replace("{{demo_link}}", url).replace("{{screenshot}}", `/screenshots/${project}.png`).replace("{{screenshot_alt}}", `${project} screenshot`),
+      type: 0
+    });
   }
-
-  const content = links.join("");
-
-  const file = html.replace("{{ content }}", content);
-
-  await writeFile(OUTPUT_HTML, file);
+  const imageContent = links.filter((e) => e.type === 0).map((e) => e.templete).join("");
+  const linkContent = links.filter((e) => e.type === 1).map((e) => e.templete).join("");
+  const html = await readFile(paths.INPUT.TEMPLETE, {
+    encoding: "utf-8"
+  });
+  const file = html.replace("{{mainContent}}", imageContent).replace("{{linksContent}}", linkContent);
+  await writeFile(paths.OUTPUTS.HTML, file);
 } catch (error) {
   console.error(error);
   process.exit(1);
---
+++ scripts/github-pages.mts
@@ -0,0 +1,92 @@
+import { readdir, writeFile, readFile, copyFile, mkdir } from "node:fs/promises";
+import { fileURLToPath } from "node:url";
+import { existsSync } from "node:fs";
+import { join } from "node:path";
+
+function reslove(path: string) {
+    return fileURLToPath(import.meta.resolve(path));
+}
+const paths = {
+    INPUT: {
+        PROJECTS: reslove("../packages"),
+        TEMPLETE: reslove("./index.html")
+    },
+    OUTPUTS: {
+        DIR: reslove("../dist"),
+        SCREENSHOTS: "screenshots",
+        HTML: reslove("index.html")
+    }
+}
+
+const imageItem = `
+    <li>
+        <div style="margin-bottom: 10px; margin-top: 20px;">
+          <span>{{project_name}} |</span>
+          <a href="{{demo_link}}" target="_blank">View Demo</a>
+        </div>
+        <img src="{{screenshot}}" alt="{{screenshot_alt}}"/>
+    </li>
+`;
+
+const linkItem = `
+  <li>
+     <div style="margin-bottom: 10px; margin-top: 20px;">
+        <span>{{project_name}} |</span>
+        <a href="{{link}}" target="_blank">View Page</a>
+      </div>
+  </li>
+`;
+
+try {
+    if (!existsSync(paths.OUTPUTS.DIR)) await mkdir(paths.OUTPUTS.DIR);
+
+    const screenshotDir = join(paths.OUTPUTS.DIR, paths.OUTPUTS.SCREENSHOTS);
+    if (!existsSync(screenshotDir)) await mkdir(screenshotDir);
+
+    const outputScreenshots = join(paths.OUTPUTS.DIR, paths.OUTPUTS.SCREENSHOTS);
+    const links: { templete: string; type: 0 | 1 }[] = [];
+    for (const project of await readdir(paths.INPUT.PROJECTS)) {
+        const url = `/${project}`;
+        const name = project
+            .split("-")
+            .map((e, i) => e.replace(/^./, e[0].toUpperCase()))
+            .join(" ");
+
+        const screenshot = join(paths.INPUT.PROJECTS, `${project}/screenshot.png`);
+        if (!existsSync(screenshot)) {
+            links.push({
+                templete: linkItem.replace("{{project_name}}", name).replace("{{link}}", url),
+                type: 1
+            });
+            continue;
+        }
+
+        await copyFile(screenshot, join(outputScreenshots, `${project}.png`));
+
+        links.push({
+            templete: imageItem.replace("{{project_name}}", name)
+                .replace("{{demo_link}}", url)
+                .replace("{{screenshot}}", `/screenshots/${project}.png`)
+                .replace("{{screenshot_alt}}", `${project} screenshot`),
+            type: 0
+        });
+    }
+
+
+    const imageContent = links.filter(e => e.type === 0).map(e => e.templete).join("");
+    const linkContent = links.filter(e => e.type === 1).map(e => e.templete).join("");
+
+
+    const html = await readFile(paths.INPUT.TEMPLETE, {
+        encoding: "utf-8",
+    });
+
+    const file = html.replace("{{mainContent}}", imageContent)
+        .replace("{{linksContent}}", linkContent);
+
+    await writeFile(paths.OUTPUTS.HTML, file);
+
+} catch (error) {
+    console.error(error);
+    process.exit(1);
+}
